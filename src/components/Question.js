7aa78fbaa02c1ab5b4fe2f14cf1c2d92574d95ba
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-07-21 12:36:46 +0300

    Assignment Done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+useEffect(()=>{
+  let intervalId = setTimeout(()=>{
+    if(timeRemaining >0){
+       setTimeRemaining(timeRemaining-1)
+    }else{
+      setTimeRemaining(10)
+      onAnswered(false)
+    } 
+  },1000)
+
+  return(
+    ()=>cleanup
+  )
+},[timeRemaining])
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
